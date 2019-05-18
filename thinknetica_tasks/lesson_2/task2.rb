a8954d5ed503484f9aa56434bf8fd83389d88ffd
Adam Cisco <47614440+ad-sys@users.noreply.github.com>
2019-05-18 22:42:37 +0200

    Updated everything

--- thinknetica_tasks/lesson_2/task2.rb
+++ thinknetica_tasks/lesson_2/task2.rb
@@ -1,16 +1,2 @@
-my_array = Array.new
-number = 0
-
-loop do
-  number += 5
-  if number >= 10
-    my_array.push(number)
-    break if number >= 100
-  end
-end
-
-for number in my_array
-  puts number
-end
-
-
+my_array = (10..100).step(5).to_a
+puts my_array
