b6821aac7c14558543ef29a097405920213ee19b
Adam Cisco <47614440+ad-sys@users.noreply.github.com>
2019-05-18 22:26:49 +0200

    Updated everything

--- thinknetica_tasks/lesson_2/task2.rb
+++ thinknetica_tasks/lesson_2/task2.rb
@@ -1,1 +1,16 @@
+my_array = Array.new
+number = 0
+
+loop do
+  number += 5
+  if number >= 10
+    my_array.push(number)
+    break if number >= 100
+  end
+end
+
+for number in my_array
+  puts number
+end
+
 
