3fcd771ae3a9bb457913cc142c61bc50d8b30678
Adam Cisco <47614440+ad-sys@users.noreply.github.com>
2019-05-19 22:29:35 +0200

    Updated everything

--- thinknetica_tasks/lesson_2/task3.rb
+++ thinknetica_tasks/lesson_2/task3.rb
@@ -1,7 +1,11 @@
-c = s = 1
+fibonacci_numbers = [0, 1]
+
 loop do
-  puts c, s
-  c += s
-  s += c
-  break if c > 89
+  next_fibonacci_number = fibonacci_numbers [-1] + fibonacci_numbers[-2]
+  break if next_fibonacci_number > 100
+
+  fibonacci_numbers.push(next_fibonacci_number)
+
 end
+
+puts fibonacci_numbers
--- thinknetica_tasks/lesson_2/task4.rb
+++ thinknetica_tasks/lesson_2/task4.rb
@@ -1,3 +1,16 @@
-vowels = ["a", "e", "i", "o", "u"]
-hash = Hash[(vowels).to_a.zip((1..5).to_a)]
-puts hash
+alphabet_hash = {}
+vowels_hash = {}
+
+alphabet = ('a'..'z').to_a
+
+vowels = %w[a e i o u]
+
+alphabet.each.with_index(1) { |letter, index| alphabet_hash[letter] = index }
+
+puts alphabet_hash
+
+alphabet.each_with_index do |letter, index|
+  vowels_hash[letter] = index + 1 if vowels.include?(letter)
+end
+
+puts vowels_hash
