ae2c8bb13eb0f86c3b1077e124b696964f3810c2
Adam Cisco <47614440+ad-sys@users.noreply.github.com>
2019-05-21 11:27:40 +0200

    Updated everything

--- thinknetica_tasks/lesson_2/nth_day_of_the_year_index.rb
+++ thinknetica_tasks/lesson_2/nth_day_of_the_year_index.rb
@@ -1,28 +1,23 @@
-#Заданы три числа, которые обозначают число, месяц, год (запрашиваем у пользователя).
-# Найти порядковый номер даты, начиная отсчет с начала года. Учесть, что год может быть високосным.
-# (Запрещено использовать встроенные в ruby методы для этого вроде Date#yday или Date#leap?)
-# Алгоритм опредления високосного года: http://www.adm.yar.ru/arch_serv/2000/Microsoft/content/vis.htm
 print 'Day (DD): '
-day = gets.chomp.to_f
+day = gets.chomp.to_i
 print 'Month (MM): '
-month = gets.chomp.to_f
+month = gets.chomp.to_i
 print 'Year (YYY): '
-year = gets.chomp.to_f
+year = gets.chomp.to_i
 
 months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-leap_year = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
+leap_year = (year % 4.0 == 0) && (year % 100.0 != 0) || (year % 400.0 == 0)
 if leap_year
   months[1] = 29
 end
 
 days_gone = 0
-months_gone = month.to_i - 2
+
+months_gone = month == 1 ? 0 : (month - 2)
 
 months[0..months_gone].each { |i| days_gone +=i }
 
 count = day.to_i + days_gone
-
-
 
 puts "From the beggining of the year: #{count} days passed."
