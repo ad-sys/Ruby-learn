f46243684302f7e49179c34124b94abdf449910a
ad-sys <adam@jetthoughts.com>
2019-07-16 18:03:56 +0200

    upd

--- thinknetica_tasks/lesson_5/manufacturer_info.rb
+++ thinknetica_tasks/lesson_5/manufacturer_info.rb
@@ -1,9 +1,5 @@
-require_relative 'main.rb'
-
 module ManufacturerInfo
 
-  def assign_manufacturer_name(object) #train or wagon
-    object.send('manufacturer name=', value)
-  end
+  attr_accessor :manufacturer
 
 end
--- thinknetica_tasks/lesson_5/wagon.rb
+++ thinknetica_tasks/lesson_5/wagon.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+#
+require_relative ‘manufacturer_info’
 
 class Wagon
   include ManufacturerInfo
