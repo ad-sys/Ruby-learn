e82a99107d68dea0f24e1171ff01583833670ad9
ad-sys <adam@jetthoughts.com>
2019-07-16 17:34:57 +0200

    upd

--- thinknetica_tasks/lesson_5/manufacturer_info.rb
+++ thinknetica_tasks/lesson_5/manufacturer_info.rb
@@ -2,15 +2,8 @@ require_relative 'main.rb'
 
 module ManufacturerInfo
 
-  def self.included(base)
-    base.send :include, InstanceMethods
-  end
-
-  InstanceMethods
-
   def assign_manufacturer_name(object) #train or wagon
-
-  object.send('manufacturer name=', value)
+    object.send('manufacturer name=', value)
   end
 
 end
