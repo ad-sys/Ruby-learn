123cc256905f421721b6c1b0e0fa80ec6c6f1bc4
ad-sys <adam@jetthoughts.com>
2019-07-18 21:57:02 +0200

    upd

--- thinknetica_tasks/lesson_5/counters.rb
+++ thinknetica_tasks/lesson_5/counters.rb
@@ -1,5 +1,16 @@
 module Counters
+
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
   @@instances = 0
+
+  module ClassMethods
+
+  end
+  module InstanceMethods
 
   def instances
     @@instances
