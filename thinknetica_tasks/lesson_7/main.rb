1e78e0b3a36a180080c3401ac31d1adf9fa27d01
ad-sys <adam@jetthoughts.com>
2019-11-05 13:59:01 +0100

    updated

--- thinknetica_tasks/lesson_7/main.rb
+++ thinknetica_tasks/lesson_7/main.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
+require_relative 'route'
+require_relative 'station'
+require_relative 'train'
 require_relative 'cargo_train'
 require_relative 'cargo_wagon'
 require_relative 'passenger_train'
 require_relative 'passenger_wagon'
-require_relative 'route'
-require_relative 'station'
-require_relative 'train'
 
 class Main
   attr_reader :trains, :routes, :stations
